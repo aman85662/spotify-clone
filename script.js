fb5730685e652156c5e6ef97870774081658ad40
aman85662 <118710449+aman85662@users.noreply.github.com>
2024-07-25 16:52:32 +0530

    js advance

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 console.log("welcome to spotify");
+let audio=[
+    {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"},
+    {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"},
+    {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"},
+    {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"},
+    {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"},
+    {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"},
+    {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"},
+    {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"}
+]
 let audioElement = new Audio('1.mp3');
