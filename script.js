1f23186f7f4aa3c08f46e33f3b5b0584cf6b84e3
aman85662 <118710449+aman85662@users.noreply.github.com>
2024-07-26 22:18:38 +0530

    js work

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log("welcome to spotify");
+//initializing the variables
+
 let audio=[
     {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"},
     {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"},
@@ -10,3 +12,7 @@ let audio=[
     {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"}
 ]
 let audioElement = new Audio('1.mp3');
+// audioElement.play();
+
+//listen to event
+document.addEventListener('time')
