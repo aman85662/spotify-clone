3c0fe1a7168fe13239c48d6b3b85e1fba0141e15
aman85662 <118710449+aman85662@users.noreply.github.com>
2024-07-28 14:58:37 +0530

    minor commit

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 console.log("welcome to spotify");
 //initializing the variables
+let songIndex=0;
+let audioElement = new Audio('1.mp3');
+let masterplay = document.getElementById('masterplay');
+let MyProgressBar= document.getElementById('MyProgressBar');
+
+
 
 let audio=[
     {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"},
@@ -11,8 +17,15 @@ let audio=[
     {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"},
     {songName: "let me love you",filePath: "1.mp3",coverPage: "onandon.jpg"}
 ]
-let audioElement = new Audio('1.mp3');
 // audioElement.play();
 
+//handel play pause
+masterplay.addEventListener('click',()=>{
+    if(audioElement.paused||audioElement.currentTime<=0){
+        audioElement.play;
+    }
+})
 //listen to event
-document.addEventListener('time')
+MyProgressBar.addEventListener('timeupdate',()=>{
+    consol.log('timeupdate')
+});
